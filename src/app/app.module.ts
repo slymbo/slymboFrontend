081d9a2f4713ac4f390b28abd9d30189348a6c51
slymbo <hatemslimiofficial@gmail.com>
2022-12-23 22:34:36 +0100

    component left center right

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,19 @@ import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
 import { FooterComponent } from './footer/footer.component';
 import { ContentComponent } from './content/content.component';
+import { RightComponent } from './content/right/right.component';
+import { CenterComponent } from './content/center/center.component';
+import { LeftComponent } from './content/left/left.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavbarComponent,
     FooterComponent,
-    ContentComponent
+    ContentComponent,
+    RightComponent,
+    CenterComponent,
+    LeftComponent
   ],
   imports: [
     BrowserModule,
